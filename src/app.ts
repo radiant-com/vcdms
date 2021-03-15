04b32527a1747c8dc8b5136817ca4a654d77f90b
Prashant <prashantmalik@techcompiler.com>
2021-03-15 16:08:27 +0530

    swagger implementation

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,59 @@
 import express, { Request, Response, NextFunction } from 'express';
 import * as bodyParser from 'body-parser';
 import "reflect-metadata";
+import * as swagger from 'swagger-express-ts';
+import { SwaggerDefinitionConstant } from 'swagger-express-ts';
 import { appAPI } from './routes/apiroutes';
 
 /**
  * @description Express server application class.
  */
 class App {
-  public server = express();
+public server = express();
 
-  constructor() {
-    this.defineRoutes();
-  }
+constructor() {
+  this.initMiddlewares();
+  this.defineRoutes();
+}
 
-  private defineRoutes(): void {
+private initMiddlewares(): void {
+  this.server.use('/api-docs/swagger', express.static('swagger'));
+  this.server.use('/api-docs/swagger/assets', express.static('node_modules/swagger-ui-dist'));
+  this.server.use(bodyParser.json());
+  this.server.use(swagger.express({
+    definition: {
+      info: {
+        title: "VCDMS APIs",
+        version: "1.0",
+      },
+      securityDefinitions: {
+        apiKeyHeader: {
+          type: SwaggerDefinitionConstant.Security.Type.API_KEY,
+          in: SwaggerDefinitionConstant.Security.In.HEADER,
+          name: "Authorization"
+        }
+      }
+    }
+  }));
+}
 
-    // API Base path
-    this.server.use(bodyParser.urlencoded({ extended: false }));
-    this.server.use(bodyParser.json());
-    this.server.use(appAPI.path, appAPI.routerinstance);
-    
-    // fallback invalid route
-    this.server.use((req: Request, res: Response, next: NextFunction) => {
-      res.status(404).json({
-        success: false,
-        message: 'Invalid route',
-        result: {},
-        statusCode: 404
-      });
+private defineRoutes(): void {
+
+  // API Base path
+  this.server.use(bodyParser.urlencoded({ extended: false }));
+  this.server.use(bodyParser.json());
+  this.server.use(appAPI.path, appAPI.routerinstance);
+
+  // fallback invalid route
+  this.server.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+      success: false,
+      message: 'Invalid route',
+      result: {},
+      statusCode: 404
     });
-  }
+  });
+}
 
 }
 
--- src/controllers/v1/encoder/encodercontrollerv1.ts
+++ src/controllers/v1/encoder/encodercontrollerv1.ts
@@ -1,14 +1,36 @@
 import { NextFunction, Response } from 'express';
 import { BaseController } from '../../basecontroller';
-import {encoderServicesV1} from '../../../services/v1/encoder/encoderservicesv1';
-import {ReqEncoderschemas} from '../../../routes/v1/encoder/encoderschema';
+import { encoderServicesV1 } from '../../../services/v1/encoder/encoderservicesv1';
+import { ReqEncoderschemas } from '../../../routes/v1/encoder/encoderschema';
 import { IFilteredRequest } from "../../../interfaces";
-
+import { ApiPath, SwaggerDefinitionConstant, ApiOperationPost } from "swagger-express-ts"
+@ApiPath({
+    path: "/api/v1",
+    name: "Distributor Process",
+    security: { apiKeyHeader: [] },
+})
 class EncoderControllerV1 extends BaseController {
 
     /**
      * @description Get currently set properties of encoder.
      */
+
+    @ApiOperationPost({
+        description: "Api to import properties using port",
+        path: '/getencoderpropertiesbyport',
+        summary: "Api to import properties using port",
+        parameters: { body: { description: "Port Number", required: true } },
+        responses: {
+            200: {
+                description: "Success",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            },
+            404: {
+                description: "Fail",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            }
+        },
+    })
     public async GetEncoderPropertiesByPort(req: IFilteredRequest<ReqEncoderschemas>, res: Response, next: NextFunction) {
         try {
             const requestResult = await encoderServicesV1.GetEncoderPropertiesByPort(req.body);
@@ -21,6 +43,22 @@ class EncoderControllerV1 extends BaseController {
     /**
      * @description Get current status of encoder.
      */
+     @ApiOperationPost({
+        description: "Api to import status using port",
+        path: '/getencoderstatusbyport',
+        summary: "Api to import status using port",
+        parameters: { body: { description: "Port Number", required: true } },
+        responses: {
+            200: {
+                description: "Success",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            },
+            404: {
+                description: "Fail",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            }
+        },
+    })
     public async GetEncoderStatusByPort(req: IFilteredRequest<ReqEncoderschemas>, res: Response, next: NextFunction) {
         try {
             const requestResult = await encoderServicesV1.GetEncoderStatusByPort(req.body);
@@ -33,6 +71,22 @@ class EncoderControllerV1 extends BaseController {
     /**
      * @description Request to start encoding.
      */
+    @ApiOperationPost({
+        description: "Api to start encoding by port ",
+        path: '/startencodingbyport',
+        summary: "Api to start encoding by port ",
+        parameters: { body: { description: "Port Number", required: true } },
+        responses: {
+            200: {
+                description: "Success",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            },
+            404: {
+                description: "Fail",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            }
+        },
+    })
     public async StartEncodingByPort(req: IFilteredRequest<ReqEncoderschemas>, res: Response, next: NextFunction) {
         try {
             const requestResult = await encoderServicesV1.StartEncodingByPort(req.body);
@@ -45,6 +99,23 @@ class EncoderControllerV1 extends BaseController {
     /**
      * @description Request to stop encoding.
      */
+
+     @ApiOperationPost({
+        description: "Api to stop encoding by port ",
+        path: '/stopencodingbyport',
+        summary: "Api to stop encoding by port ",
+        parameters: { body: { description: "Port Number", required: true } },
+        responses: {
+            200: {
+                description: "Success",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            },
+            404: {
+                description: "Fail",
+                type: SwaggerDefinitionConstant.Response.Type.OBJECT,
+            }
+        },
+    })
     public async StopEncodingByPort(req: IFilteredRequest<ReqEncoderschemas>, res: Response, next: NextFunction) {
         try {
             const requestResult = await encoderServicesV1.StopEncodingByPort(req.body);
